5036ba7c9a39379aa6dfc6be908ed4354c1c3538
irkuhs <shukriahmad56@gmail.com>
2023-05-25 11:02:03 +0800

    update api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
